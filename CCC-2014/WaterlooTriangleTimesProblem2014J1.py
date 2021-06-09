6d6f99c5bfe6ef30d52d90ffd714bd3731c55ffe
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-06-09 16:25:15 -0400

    Update WaterlooTriangleTimesProblem2014J1.py

--- CCC-2014/WaterlooTriangleTimesProblem2014J1.py
+++ CCC-2014/WaterlooTriangleTimesProblem2014J1.py
@@ -6,14 +6,19 @@ y = int(input())
 z = int(input())
 
 # series of if-statements to determine the characteristics of the triangle.
+
+# determining an equilateral triangle.
 if x and y and z == 60:
     print("Equilateral")
 
+# determining an isoceles triangle.
 elif (x + y + z == 180) and (x == y or x == z or y == z):
     print("Isoceles")
 
+# determining a scalene triangle.
 elif (x + y + z == 180) and (x != y or x != z or y != z):
     print("Scalene")
 
+# printing 'Error' if triangle is not recognized.
 else:
     print("Error")
